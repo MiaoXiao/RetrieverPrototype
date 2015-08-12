06bf4ab5847041e82c7ab3ab7134066ad98f09da
Rica Feng <topgb@msn.com>
2015-08-11 23:07:46 -0700

    added structure for battle

--- src/Battle.cpp
+++ src/Battle.cpp
@@ -1,9 +1,16 @@
 #include "Battle.h"
 
+#include <iostream>
 #include <string>
 #include <stdlib.h>
 
 using namespace std;
+
+//start battle
+bool start_Battle()
+{
+	
+}
 
 //assign battle log
 void Battle::assign_BattleLog(Player p1, Player p2, vector<Figment> figmentlist)
@@ -11,16 +18,20 @@ void Battle::assign_BattleLog(Player p1, Player p2, vector<Figment> figmentlist)
 	//add tylor and liza to battlelog 
 	Character* tylor = &p1;
 	Character* liza = &p2;	
-	battlelog.insert(pair<int, Character*>(tylor->stats.get_Haste(), tylor));
-	battlelog.insert(pair<int, Character*>(liza->stats.get_Haste(), liza));
+	battlelog.insert(pair<int, Character*>(100 - tylor->stats.get_Haste(), tylor));
+	battlelog.insert(pair<int, Character*>(100 - liza->stats.get_Haste(), liza));
 
 	//ERROR: no figment to fight
-	if (!figmentlist.size()) exit(1);	
+	if (!figmentlist.size()) 
+	{	
+		cerr << "No figment to fight. exiting" << endl;
+		exit(1);
+	}	
 
 	//add all figments to battle log
 	for (unsigned int i = 0; i < figmentlist.size(); ++i)
 	{
 		Character* figment = &figmentlist[i];
-		battlelog.insert(pair<int, Character*>(figment->stats.get_Haste(), figment));
+		battlelog.insert(pair<int, Character*>(100 - figment->stats.get_Haste(), figment));
 	}	
 }
--- src/Battle.h
+++ src/Battle.h
@@ -16,12 +16,16 @@ class Battle
 			//check avaliability of both players
 			assign_BattleLog(p1, p2, figmentlist);
 		}
+
+		//begin battle. return true if player wins
+		bool start_Battle();
 	private:
 		//multimap of battlelog
 		std::multimap<int, Character*> battlelog;
 		
 		//compare haste between players and figments. assign battlelog of turn order
 		void assign_BattleLog(Player p1, Player p2, std::vector<Figment> figmentlist);
+
 		
 };
 
