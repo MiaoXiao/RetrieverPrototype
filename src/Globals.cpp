b7e5eca9c0e8f865bdcd4e51de6ebe13a4827ad0
Rica Feng <topgb@msn.com>
2015-08-14 02:23:57 -0700

    new file, for global functions

---
+++ src/Globals.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "Globals.h"
+
+//will return true given probability, given number between 0 - 1
+bool Probability::chanceToOccur(const float p)
+{
+	if (p < 0 || p > 1) 
+	{
+		std::cerr << "Probability not between 0 and 1. Exiting." << std::endl;
+		exit(1);
+	}
+	
+	return p >= (((double) rand() / (RAND_MAX)) + 1);
+}
+
+//return random number between lower bound and higher bound exclusive
+int Probability::generateRandomNumber(const int lb, const int hb) { return rand() % hb + lb; }
